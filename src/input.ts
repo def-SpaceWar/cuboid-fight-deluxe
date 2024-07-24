c3231030346515bc382913e456623ffb658d53e6
Aryan Ahire <aryanahire9086360780@gmail.com>
2024-07-24 01:51:29 -0400

    updated mouse input

--- src/input.ts
+++ src/input.ts
@@ -1,17 +1,19 @@
 import { Vector2D } from "./math";
 
-let keys = new Set<string>;
-export let isMouseDown = false;
-let mouseCoords = Vector2D.zero();
+let keys = new Set<string>,
+    mouseButtons = new Float32Array(5),
+    mouseCoords = Vector2D.zero();
 
-export const isPressed = (k: string) => keys.has(k);
-export const getMouseCoords = () => mouseCoords.clone();
+export const
+    isPressed = (k: string) => keys.has(k),
+    isMousePressed = (btn: number) => mouseButtons[btn] == 1,
+    getMouseCoords = () => mouseCoords.clone();
 
 const listeners = {
     "keydown": (e: KeyboardEvent) => keys.add(e.key),
     "keyup": (e: KeyboardEvent) => keys.delete(e.key),
-    "mousedown": () => isMouseDown = true,
-    "mouseup": () => isMouseDown = false,
+    "mousedown": (e: MouseEvent) => mouseButtons[e.button] = 1,
+    "mouseup": (e: MouseEvent) => mouseButtons[e.button] = 0,
     "mousemove": (e: MouseEvent) => mouseCoords.sxy(e.clientX, e.clientY),
 };
 
@@ -23,8 +25,8 @@ export function listenToInput() {
 
 export function stopListeningToInput() {
     keys = new Set<string>;
+    mouseButtons = new Float32Array(5);
     mouseCoords = Vector2D.zero();
-    isMouseDown = false;
 
     for (const type of Object.keys(listeners))
         // @ts-ignore
