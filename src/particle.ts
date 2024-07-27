49331069958033a0554edcbd46742f1650940db9
Aryan Ahire <aryanahire9086360780@gmail.com>
2024-07-27 00:26:33 -0400

    fixed vercel build

--- src/particle.ts
+++ src/particle.ts
@@ -10,8 +10,10 @@ const particles: Particle[] = [];
 
 export function updateParticles(dt: number) {
     throw "not implemented yet";
+    dt;
     for (let i = 0; i < particles.length; i++) {
         const particle = particles[i];
+        particle;
     }
 }
 
@@ -19,6 +21,7 @@ export function renderParticles() {
     throw "not implemented yet";
     for (let i = 0; i < particles.length; i++) {
         const particle = particles[i];
+        particle;
     }
 }
 
